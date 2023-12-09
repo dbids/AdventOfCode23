eb07c541010fa86eb9b8464f7e5f3c8389c7c589
Devin Bidstrup <55927617+dbids@users.noreply.github.com>
2023-12-08 19:08:42 -0700

    day 9 setup

---
+++ days_pkg/day9/day9.py
@@ -0,0 +1,37 @@
+import traceback
+from aocd import submit
+from aocd import get_data
+import sys
+
+# Part A
+def part_a(dataList):
+  ans_a = 0
+  return ans_a
+
+# Part B
+def part_b(dataList):
+  ans_b = 0
+  return ans_b
+
+# Main loop
+if __name__ == "__main__":
+  day = 9
+  try:
+    # Get either puzzle input from server or sample from txt as list of strings
+    if (len(sys.argv) > 1):
+      with open('day' + str(day) + '_sample.txt') as f:
+        dataList = [line.split('\n')[0] for line in f.readlines()]
+    else:
+      dataList = get_data(day=day, year=2023).split('\n')
+    
+    ans_a = part_a(dataList=dataList)
+    print(f"ans_a:{ans_a}")
+    # submit(ans_a, part="a", day=day, year=2023)
+
+    # ans_b = part_b(dataList=dataList)
+    # print(f"ans_b:{ans_b}")
+    # submit(ans_b, part="b", day=day, year=2023)
+
+  except Exception:
+    traceback.print_exc()
+  
